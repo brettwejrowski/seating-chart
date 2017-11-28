35a83f1a21a38bf83d880cb37d099f3116189873
Brett Wejrowski <brett@wejrowski.com>
2017-11-27 22:54:37 -0800

    seat count kinda

--- web/src/components/LayoutEditor/index.jsx
+++ web/src/components/LayoutEditor/index.jsx
@@ -45,8 +45,8 @@ export default class LayoutEditor extends Component {
       return;
     }
 
-    const x = Math.round((e.clientX - drag_offset_x) / BLOCK_SIZE);
-    const y = Math.round((e.clientY - drag_offset_y) / BLOCK_SIZE);
+    const x = (e.clientX - drag_offset_x) / BLOCK_SIZE;
+    const y = (e.clientY - drag_offset_y) / BLOCK_SIZE;
 
     if (event_type == 'drag') {
       let tables = this.state.tables;
@@ -54,8 +54,22 @@ export default class LayoutEditor extends Component {
       if ((e.clientY) > (window.innerHeight - 50)) {
         tables.splice(tables.indexOf(active_table), 1);
       } else {
-        active_table.x = x;
-        active_table.y = y;
+        active_table.x = Math.round(x);
+        active_table.y = Math.round(y);
+      }
+
+      this.setState({
+        active_table: null,
+        tables: tables,
+      });
+    } else if (event_type == 'add') {
+      let tables = this.state.tables;
+
+      if ((e.clientY) < (window.innerHeight - 50)) {
+        active_table.x = Math.round(x);
+        active_table.y = Math.round(y);
+        active_table.id = `table${Math.random()}`;
+        tables.push(active_table);
       }
 
       this.setState({
@@ -63,9 +77,18 @@ export default class LayoutEditor extends Component {
         tables: tables,
       });
 
+
     } else if (event_type == 'resize') {
-      active_table.width = Math.max(1, x - active_table.x + active_table.width);
-      active_table.height = Math.max(1, y - active_table.y + active_table.height);
+      let width = Math.max(1, x - active_table.x + active_table.width);
+      let height = Math.max(1, y - active_table.y + active_table.height);
+
+      if (active_table.table_type == 'circle') {
+        let diameter = Math.min()
+        active_table.number_of_seats = Math.floor(Math.PI * Math.min(width, height));
+      }
+
+      active_table.width = Math.round(Math.min(width, height));
+      active_table.height = active_table.width;
       this.setState({
         active_table: null,
         tables: this.state.tables,
@@ -83,6 +106,16 @@ export default class LayoutEditor extends Component {
 
   startResizing (table, e) {
     this.startEvent('resize', table, e);
+  }
+
+  startAdding (table, e) {
+    this.startEvent('add', {
+      table_type: table.table_type || 'rect',
+      width: table.width,
+      height: table.height,
+      x: (e.clientX / BLOCK_SIZE) - (table.width / 2),
+      y: (e.clientY / BLOCK_SIZE) - (table.height / 2),
+    }, e);
   }
 
   dragMove (e) {
@@ -179,6 +212,60 @@ export default class LayoutEditor extends Component {
     )
   }
 
+  renderPalette () {
+    return (
+      <div className={localStyles.palette}>
+        <div className={localStyles.plus}>
+          +
+        </div>
+
+        <div className={localStyles.drawer}>
+          <Table
+            blockSize={13}
+            x={3}
+            y={2}
+            width={2}
+            height={4}
+            type='rect'
+            startDragging={(e) => this.startAdding({
+              width: 2,
+              height: 4,
+              table_type: 'rect',
+            }, e)}
+          />
+
+          <Table
+            blockSize={13}
+            x={10}
+            y={3}
+            width={4}
+            height={2}
+            type='rect'
+            startDragging={(e) => this.startAdding({
+              width: 3,
+              height: 4,
+              table_type: 'rect',
+            }, e)}
+          />
+
+          <Table
+            blockSize={13}
+            x={19}
+            y={2.5}
+            width={3}
+            height={3}
+            type='circle'
+            startDragging={(e) => this.startAdding({
+              width: 3,
+              height: 3,
+              table_type: 'circle',
+            }, e)}
+          />
+        </div>
+      </div>
+    );
+  }
+
   render () {
     const {
       tables,
@@ -196,14 +283,16 @@ export default class LayoutEditor extends Component {
     if (active_table) {
       if (event_type == 'resize') {
         changing_table = {
+          table_type: active_table.table_type,
           x: active_table.x,
           y: active_table.y,
           width: Math.max(1, (mouse_x / BLOCK_SIZE) - active_table.x),
           height: Math.max(1, (mouse_y / BLOCK_SIZE) - active_table.y),
         };
 
-      } else if (event_type == 'drag') {
+      } else if (event_type == 'drag' || event_type == 'add') {
         changing_table = {
+          table_type: active_table.table_type,
           x: (mouse_x - drag_offset_x) / BLOCK_SIZE,
           y: (mouse_y - drag_offset_y) / BLOCK_SIZE,
           width: active_table.width,
@@ -237,6 +326,7 @@ export default class LayoutEditor extends Component {
             y={table.y}
             width={table.width}
             height={table.height}
+            type={table.table_type}
             className={cx(
               !!active_table && table.id === active_table.id && 'dragstart',
             )}
@@ -251,13 +341,16 @@ export default class LayoutEditor extends Component {
             className={event_type}
             x={changing_table.x}
             y={changing_table.y}
+            type={changing_table.table_type}
+            numberOfSeats={changing_table.number_of_seats}
             dropToDelete={changing_table.drop_to_delete}
             width={changing_table.width}
             height={changing_table.height}
           />
         }
 
-        {this.renderTrash(!!changing_table && event_type === 'drag')}
+        {this.renderTrash(!!changing_table && (event_type === 'drag' || event_type === 'add'))}
+        {this.renderPalette()}
       </div>
     );
   }
@@ -276,49 +369,74 @@ class Table extends Component {
 
   render () {
     const {
+      type,
       blockSize,
-      x, y, width, height,
+      x, y,
       deletedChairs,
       className,
       startDragging,
       startResizing,
       dropToDelete,
+      numberOfSeats,
     } = this.props;
 
+    let { width, height } = this.props;
+    if (type == 'circle') {
+      width = Math.min(width, height);
+      height = width;
+    }
 
     const chairs = [];
 
-    for (let x_iter = 1; x_iter < width - 0.5; x_iter++) {
-      chairs.push({
-        x: x_iter - 0.5,
-        y: height,
-        rotate: '180deg',
-        id: 'S' + x_iter,
-      },{
-        x: x_iter - 0.5,
-        y: -1,
-        rotate: '0',
-        id: 'N' + x_iter,
-      });
-    }
+    if (type == 'rect') {
+      for (let x_iter = 1; x_iter < width - 0.5; x_iter++) {
+        chairs.push({
+          x: x_iter - 0.5,
+          y: height,
+          rotate: '180deg',
+          id: 'S' + x_iter,
+        },{
+          x: x_iter - 0.5,
+          y: -1,
+          rotate: '0',
+          id: 'N' + x_iter,
+        });
+      }
 
-    for (let y_iter = 1; y_iter < height - 0.5; y_iter++) {
-      chairs.push({
-        y: y_iter - 0.5,
-        x: width,
-        rotate: '90deg',
-        id: 'E' + y_iter,
-      },{
-        y: y_iter - 0.5,
-        x: -1,
-        rotate: '-90deg',
-        id: 'W' + y_iter,
-      });
+      for (let y_iter = 1; y_iter < height - 0.5; y_iter++) {
+        chairs.push({
+          y: y_iter - 0.5,
+          x: width,
+          rotate: '90deg',
+          id: 'E' + y_iter,
+        },{
+          y: y_iter - 0.5,
+          x: -1,
+          rotate: '-90deg',
+          id: 'W' + y_iter,
+        });
+      }
+    } else if (type == 'circle') {
+      let chair_count = numberOfSeats || Math.floor((Math.PI * width));
+      let rads = (2 * Math.PI) / chair_count;
+
+      for (let chair_iter = 0; chair_iter < chair_count; chair_iter++ ) {
+        let rad = chair_iter * rads;
+
+        chairs.push({
+          x: (width / 2 + 0.5) * Math.cos(rad) + (width / 2) - 0.5,
+          y: (width / 2 + 0.5) * Math.sin(rad) + (width / 2) - 0.5,
+          rotate: `${rad + Math.PI / 2}rad`,
+          id: `D${rad}`,
+        })
+      }
+
     }
 
     return (
       <div
         className={cx(
+          localStyles[type],
           localStyles.wrapper,
           !!dropToDelete && 'drop-to-delete',
           className,
@@ -365,6 +483,15 @@ class Table extends Component {
             <div className={localStyles.chairback} />
           </div>
         )}
+
+        <div
+          style={{
+            'top': `-${blockSize + 15}px`,
+            'left': `-${blockSize}px`,
+          }}
+          className={localStyles.count}>
+          {chairs.length} seat{chairs.length !== 1 && 's'}
+        </div>
       </div>
     );
   }
